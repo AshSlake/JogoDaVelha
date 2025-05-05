dd2c2a8c9133530b46916cd18b650ae35f198b9a
Paulo Henrique <127267437+AshSlake@users.noreply.github.com>
2025-05-05 09:07:02 -0300

    Adicionar arquivos de projeto.

---
+++ JogoDaVelha/MainPage.xaml.cs
@@ -0,0 +1,48 @@
+﻿namespace JogoDaVelha
+{
+    public partial class MainPage : ContentPage
+    {
+        string vez = "X";
+
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Clicked(object sender, EventArgs e)
+        {
+            Button btn = (Button)sender;
+            btn.IsEnabled = false;
+            if (vez == "X")
+            {
+                btn.Text = "X";
+                vez = "O";
+            }
+            else
+            {
+                btn.Text = "O";
+                vez = "X";
+            }
+
+            /*verificação se X ganhou na primeira linha*/
+            if (btn1_0.Text == "X" && btn1_1.Text == "X" && btn1_2.Text == "X")
+            {
+                DisplayAlert("Parabens", "X ganhou", "OK");
+                Zerar();
+            }
+
+        }
+
+        void Zerar()
+        {
+            btn1_0.Text = "";
+            btn1_1.Text = "";
+            btn1_2.Text = "";
+
+            btn1_0.IsEnabled = true;
+            btn1_1.IsEnabled = true;
+            btn1_2.IsEnabled = true;
+        }
+    }
+
+}
